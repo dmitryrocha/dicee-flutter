18388f4846e4936b22bd7cd49cd7919952433d97
Dmitry Rocha <dmitryrocha@gmail.com>
2020-09-14 16:13:14 -0300

    Projeto finalizado
    Um botão extra para girar os dados simultaneamente foi criado

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   return runApp(
@@ -15,9 +18,83 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+
+    int changeNumber(){
+      return Random().nextInt(6)+1;
+    }
+
+
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: FlatButton(
+                  color: Colors.white,
+                      textColor: Colors.red,
+                      padding: EdgeInsets.all(8.0),
+                      splashColor: Colors.redAccent,
+                      onPressed: () {
+                        setState(() {
+                          leftDiceNumber = changeNumber();
+                          rightDiceNumber = changeNumber();
+                        });
+                      },
+                  child: Text(
+                    "Rodar ambos os dados simutaneamente",
+                    style: TextStyle(fontSize: 20.0),
+                  )
+                  ),
+                )
+            ],
+          ),
+          Row(
+            children: [
+              SizedBox(
+                height: 20.0,
+              ),
+            ],
+          ),
+          Row(
+            children:<Widget> [
+              Expanded(
+                child: FlatButton(
+                    onPressed: (){
+                      setState(() {
+                        leftDiceNumber = changeNumber();
+                      });
+                    },
+                    child: Image.asset('images/dice$leftDiceNumber.png')
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    setState(() {
+                      rightDiceNumber = changeNumber();
+                    });
+                  },
+                    child: Image.asset('images/dice$rightDiceNumber.png')
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
+
